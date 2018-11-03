dee35bae878b74561ec2fdae267953dc5836891d
BoucheDimitri <dimitri.bouche@ensae-paristech.fr>
2018-11-03 19:42:29 +0100

    All good for part 1

--- main.py
+++ main.py
@@ -7,6 +7,8 @@ import dynamic_prog as dp
 importlib.reload(dp)
 
 
+# ########################## Question 1.1 #########################################################################
+
 # Number of states
 nstates = 3
 
@@ -46,13 +48,15 @@ rvecs[0][2] = 5 / 100
 rvecs[2][1] = 1
 rvecs[2][2] = 9 / 10
 
-
+# Set discount parameter to 0.95
 gamma = 0.95
 
-
+# The optimal policy vector that we have guessed and its corresponding value vector
 pi_star = np.array([1, 1, 2])
 v_star = dp.policy_evaluation(rvecs, pmats, pi_star, gamma)
 
+
+# ########################## Question 1.2 #########################################################################
 
 # Question 1.2: Value iteration
 # Set nitial values to 0
@@ -68,7 +72,8 @@ plt.ylabel("$||v - v^*||_{\infty}$")
 plt.xlabel("Value iteration")
 
 
-# Question 1.3: Policy iteration
+# ########################## Question 1.3 #########################################################################
+
 # Initialize pi0 to [a0, a0, a0]
 pi0 = np.zeros((nstates, ), dtype=int)
 # Perform policy iteration and record value vectors history in hist_pit
